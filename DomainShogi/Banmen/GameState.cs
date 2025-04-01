59f062fbb08192db1a2630cada3c78705053d255
nkawarai <studio105.xxx@gmail.com>
2025-04-01 10:48:34 +0900

    局面インターフェイスの追加。

---
+++ DomainShogi/Banmen/GameState.cs
@@ -0,0 +1,14 @@
+﻿namespace DomainShogi.Banmen
+{
+    /// <summary>
+    /// 対局状態
+    /// </summary>
+    public enum GameState
+    {
+        Init,
+        OnGoing,
+        End,
+        DrawGame,
+        Resume
+    }
+}
---
+++ DomainShogi/Banmen/IKyokumen.cs
@@ -0,0 +1,37 @@
+﻿using DomainShogi.Koma;
+using DomainShogi.Sashites;
+
+namespace DomainShogi.Banmen
+{
+    /// <summary>
+    /// 局面
+    /// </summary>
+    public interface IKyokumen
+    {
+        /// <summary>
+        /// 対局状態
+        /// </summary>
+        GameState State { get; }
+
+        /// <summary>
+        /// 盤上の駒
+        /// </summary>
+        IEnumerable<IKoma> KomasOnBoard { get; }
+
+        /// <summary>
+        /// 先手の持ち駒
+        /// </summary>
+        IEnumerable<IKoma>? SenteMochigoma { get; }
+
+        /// <summary>
+        /// 後手の持ち駒
+        /// </summary>
+        IEnumerable<IKoma>? GoteMochigoma { get; }
+
+        /// <summary>
+        /// 駒を動かす
+        /// </summary>
+        /// <returns></returns>
+        IKyokumen Move(ISashite sashite);
+    }
+}
